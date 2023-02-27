081630054f88a60de5538ff8fb0848f4718344a3
Basil-Kiselev <basiliento@gmail.com>
2023-02-27 15:05:13 +0600

    Реализована синхронизация групп товаров с Мойсклад

--- app/Services/ProductfolderService.php
+++ app/Services/ProductfolderService.php
@@ -2,19 +2,39 @@
 
 namespace App\Services;
 
+use App\Exceptions\FailMoyskladException;
 use App\Models\Productfolder;
 use App\Services\Dto\ProductfolderCreateDto;
 use App\Services\Dto\ProductfolderUpdateDto;
+use Illuminate\Support\Facades\DB;
 
 class ProductfolderService
 {
     public function create(ProductfolderCreateDto $dto): Productfolder
-    {
-        return Productfolder::query()->create([
+    {   
+        DB::beginTransaction();
+        $data = [
             'name' => $dto->getName(),
             'code' => $dto->getCode(),
             'description' => $dto->getDescription(),
+        ];
+
+        $localProductfolder = Productfolder::query()->create($data);
+        $skladProductfolder = (new MoyskladClient())->createProductfolder($data);
+
+        if($skladProductfolder->failed()) {
+            DB::rollBack();
+            throw new FailMoyskladException();
+        }
+
+        $productfolderResponse = json_decode($skladProductfolder, true);
+        $localProductfolder->update([
+            'sklad_id' => $productfolderResponse['id'],
         ]);
+
+        DB::commit();
+
+        return $localProductfolder;        
     }
     
     public function read($id): Productfolder
@@ -23,19 +43,41 @@ class ProductfolderService
     }
     
     public function update(ProductfolderUpdateDto $dto): Productfolder
-    {
-        $result = Productfolder::query()->find($dto->getId());
-        $result->update([
+    {   
+        DB::beginTransaction();
+        $data = [
             'name' => $dto->getName(),
             'code' => $dto->getCode(),
             'description' => $dto->getDescription(),
-        ]);
+        ];        
+        $localProductfolder = Productfolder::query()->find($dto->getId());
+        $localProductfolder->update($data);
+        $skladProductfolder = (new MoyskladClient())->updateProductfolder($localProductfolder['sklad_id'], $data);
 
-        return $result;
+        if($skladProductfolder->failed()) {
+            DB::rollBack();
+            throw new FailMoyskladException();
+        }
+
+        DB::commit();
+
+        return $localProductfolder;        
     }
     
     public function delete($id): bool
-    {
-        return Productfolder::query()->where('id', $id)->delete();
+    {   
+        DB::beginTransaction();
+        $skladId = Productfolder::query()->where('id', $id)->value('sklad_id');
+        $localProductfolder = Productfolder::query()->where('id', $id)->delete();
+        $skladProductfolder = (new MoyskladClient())->deleteProductfolder($skladId);
+
+        if($skladProductfolder->failed()) {
+            DB::rollBack();
+            throw new FailMoyskladException();
+        }
+
+        DB::commit();
+
+        return $localProductfolder;
     }
 }
